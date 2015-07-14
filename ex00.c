eeb1308bd7ff977bfe37c4a2fce2db3094b8323b
rcouet <rcouet@student.42.fr>
2015-07-14 14:47:52 +0200

    ajout ex00

---
+++ ex00.c
@@ -0,0 +1,22 @@
+
+#include <unistd.h>
+char ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+char *ft_strcpy(char *dest, char *src)
+{
+	char dest[] = char src[];
+}
+
+char main();
+{
+	char dest[] = test;
+	char src[] = icules;
+	ft_putchar(scr);
+	ft_putchar(dest);
+	ft_strcpy(destm src);
+	ft_putchar(src);
+	ft_putchar(dest);
+}
